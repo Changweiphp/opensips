f753d93be820efa63885fb8198e4af94ce10ce83
ggz <ggz@example.com>
2014-12-08 08:53:53 +0800

    创维接口新增

--- opensips/opensips/Home/Model/UserModel.class.php
+++ opensips/opensips/Home/Model/UserModel.class.php
@@ -2,5 +2,14 @@
 namespace Home\Model;
 use Think\Model;
 class UserModel extends Model {
+	protected $tableName = 'user';
 
+	/**
+	 * 字段映射
+	 */
+	protected $_map = array(
+        'AccountId' =>'name', 
+        'pass'  =>'pwd',
+    
+    );
 }
