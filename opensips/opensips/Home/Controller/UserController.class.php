4f896eed89ab83da432a60362eb849e3da26b7c3
ggz <ggz@example.com>
2014-12-08 09:34:08 +0800

    显示表整合

--- opensips/opensips/Home/Controller/UserController.class.php
+++ opensips/opensips/Home/Controller/UserController.class.php
@@ -2,6 +2,9 @@
 namespace Home\Controller;
 use Think\Controller;
 class UserController extends Controller {
+	/**
+	 * 初始页面
+	 */
     public function index(){
     	$this->name="post";
     	$this->display();
